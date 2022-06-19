8767378c89f71c0c71f31620fb40dc52cfcbfeb2
Nipun Rautela <nipunrautela261@gmail.com>
2022-06-19 15:48:12 +0530

    Created the weather app using openweathermap API

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Web Development Projects
-Projects are separated into branches, choose a branch to view the respective project.
+# Weather App
+This is a Weather App Created using Node.js and openweathermap API.
+
+## How to run
+1. Get your API key from openweathermap.org
+2. Replace the apiKey constant inside app.js
+3. Install required packages through `npm install`
+4. Run the app through `node app.js`
---
+++ app.js
@@ -0,0 +1,47 @@
+//jshint esversion:6
+
+const https = require("https");
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+// API key for openweathermap API
+const apiKey = "6bb50b383d5206e623f100ec0f52cc4f";
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  console.log(req.body.cityName);
+
+  const query = req.body.cityName;
+  const units = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+units+"";
+  https.get(url, function(response) {
+    console.log(response.statusCode);
+
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const iconURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
+
+      res.write("<p>The weather is currently: "+weatherDescription+"</p>");
+      res.write("<h1>The temp in "+ query +" is " + temp + " degrees Celcius.</h1>");
+      res.write("<img src="+iconURL+" alt='weather icon'>");
+
+      res.send();
+    });
+  });
+
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Weather App Server started on port 3000");
+});
