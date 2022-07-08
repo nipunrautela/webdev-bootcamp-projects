d3dc3607de1e4e9903e0e4b808c19fc6877bf044
Nipun Rautela <nipunrautela261@gmail.com>
2022-07-08 18:25:06 +0530

    added feature to fetch images of the shows and display them in the body

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Web Development Projects
-Projects are separated into branches, choose a branch to view the respective project.
+# TV Shows Search App
+
+A TV Show search app made using [tvmaze](https://tvmaze.com) API.
+For HTTP requests, axios is used, docs for which can be found [here](https://github.com/axios/axios).
---
+++ app.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("#searchForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+
+  const config = {
+    params: {
+      q: searchTerm
+    }
+  };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  // console.log(res.data);
+  makeImages(res.data);
+  form.reset();
+});
+
+const makeImages = (shows) => {
+  for(let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("img");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+}
