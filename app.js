3860a53efb13fa91a670a263a627267b3090f22e
Nipun Rautela <nipunrautela261@gmail.com>
2022-10-29 12:41:59 +0530

    level 3(Hashing) security added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -25,10 +26,12 @@ const userSchema = mongoose.Schema({
 
 const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt, {
-  secret: secret,
-  encryptedFields: ["password"],
-});
+// Uncomment to activate db encryption using mongoose
+//
+// userSchema.plugin(encrypt, {
+//   secret: secret,
+//   encryptedFields: ["password"],
+// });
 
 const User = mongoose.model("User", userSchema);
 
@@ -47,7 +50,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save(function (err) {
@@ -62,7 +65,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
